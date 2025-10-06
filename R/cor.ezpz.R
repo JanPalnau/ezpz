60b997f8a011617b03db89c25f1b09289b9eb595
JanPalnau <163874303+JanPalnau@users.noreply.github.com>
2025-10-06 22:25:44 +0200

    Initial commit

---
+++ R/cor.ezpz.R
@@ -0,0 +1,108 @@
+usethis::create_package("ezpz")
+
+library(weights)
+library(boot)
+library(dplyr)
+
+cor.ezpz <- function(data,
+                     method = "pearson",
+                     cis = TRUE,
+                     weight = NULL,
+                     nboot = 1000) {
+  library(weights)
+  library(boot)
+  
+  # Separate variables from weight column
+  if (!is.null(weight)) {
+    w <- data[[weight]]
+    vars <- setdiff(colnames(data), weight)  # exclude weight variable
+  } else {
+    vars <- colnames(data)
+    w <- NULL
+  }
+  
+  n <- length(vars)
+  
+  # matrices
+  cor_matrix      <- matrix(NA, n, n, dimnames = list(vars, vars))
+  p_matrix        <- matrix(NA, n, n, dimnames = list(vars, vars))
+  ci_std_matrix   <- matrix("", n, n, dimnames = list(vars, vars))
+  ci_boot_matrix  <- matrix("", n, n, dimnames = list(vars, vars))
+  stars_matrix    <- matrix("", n, n, dimnames = list(vars, vars))
+  
+  assign_stars <- function(p) {
+    if (is.na(p)) return("")
+    if (p < .001) return("***")
+    if (p < .01)  return("**")
+    if (p < .05)  return("*")
+    ""
+  }
+  
+  # loop over pairs
+  for (i in 1:n) {
+    for (j in 1:i) {  # lower triangle only
+      if (i == j) next  # skip diagonal
+      
+      x <- data[[vars[i]]]
+      y <- data[[vars[j]]]
+      
+      if (is.null(weight)) {
+        # --- unweighted ---
+        test <- suppressWarnings(cor.test(x, y, method = method))
+        r <- unname(test$estimate)
+        pval <- test$p.value
+        ci_std <- test$conf.int
+        
+        # bootstrap CIs
+        boot_fun <- function(d, idx) {
+          cor(d[idx,1], d[idx,2], method = method, use = "pairwise")
+        }
+        boot_obj <- try(boot(data.frame(x, y), boot_fun, R = nboot), silent = TRUE)
+        ci_boot <- tryCatch({
+          bci <- boot.ci(boot_obj, type = "perc")
+          if (!is.null(bci$percent)) bci$percent[4:5] else NA
+        }, error = function(e) NA)
+        
+      } else {
+        # --- weighted ---
+        wc <- suppressWarnings(wtd.cor(x, y, weight = w, mean1 = TRUE, bootse = FALSE))
+        r <- unname(wc[1])
+        pval <- unname(wc[4])
+        
+        ci_std <- NA  # no analytic CI for weighted
+        # bootstrap for weighted
+        boot_fun <- function(d, idx) {
+          wtd.cor(d[idx,1], d[idx,2], weight = d[idx,3], mean1 = TRUE)[1]
+        }
+        boot_obj <- try(boot(cbind(x, y, w), boot_fun, R = nboot), silent = TRUE)
+        ci_boot <- tryCatch({
+          bci <- boot.ci(boot_obj, type = "perc")
+          if (!is.null(bci$percent)) bci$percent[4:5] else NA
+        }, error = function(e) NA)
+      }
+      
+      # fill matrices
+      cor_matrix[i,j] <- r
+      p_matrix[i,j]   <- pval
+      stars_matrix[i,j] <- assign_stars(pval)
+      
+      if (!is.null(ci_std) && all(is.finite(ci_std))) {
+        ci_std_matrix[i,j] <- paste0("[", round(ci_std[1],2), ", ", round(ci_std[2],2), "]")
+      }
+      if (!is.null(ci_boot) && all(is.finite(ci_boot))) {
+        ci_boot_matrix[i,j] <- paste0("[", round(ci_boot[1],2), ", ", round(ci_boot[2],2), "]")
+      }
+    }
+  }
+  
+  # combine correlations and stars
+  cor_with_stars <- ifelse(is.na(cor_matrix), "",
+                           paste0(round(cor_matrix, 2), stars_matrix))
+  
+  return(list(cor = cor_matrix,
+              p = p_matrix,
+              stars = stars_matrix,
+              cor_with_stars = cor_with_stars,
+              ci_std = ci_std_matrix,
+              ci_boot = ci_boot_matrix))
+}
